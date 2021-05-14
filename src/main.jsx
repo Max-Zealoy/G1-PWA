15c754176439fbe7d6b668a98cf8d76c9ada971c
unknown <mxzetterlund@gmail.com>
2021-05-14 12:00:05 +0200

    A new beginning/skeleton files
    Added a base with an example manifest and a basic service worker with cache.

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/serviceWorker.js')
+    .then(reg => console.log('service worker registered', reg))
+    .catch(err => console.log('service worker not registered', err));
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
