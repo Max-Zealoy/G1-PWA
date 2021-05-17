611d06da96930b5d879e885d3ea5124c562f52e6
simonperssonnn <persson_simon@outlook.com>
2021-05-17 14:32:54 +0200

    footer på gång

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styling/footer.css';
+
+
+function Footer(){
+    return(
+        <div className="FooterSize">
+            <h1>-CATSNAP-</h1>
+        </div>
+    )
+}
+
+
+export default Footer
