118ff95ac8ebd44d855d2631c8305a3fd4da2815
simonperssonnn <persson_simon@outlook.com>
2021-05-18 16:07:24 +0200

    footern blev lite bättre nu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,12 @@ function Footer(){
     return(
         <div className="FooterSize">
             <h1>-CATSNAP-</h1>
+            <h4>
+            About us Blog Job Help Confidentiality Condition Popular cats
+</h4>
+<h4>
+© 2021 Catsnap
+</h4>
         </div>
     )
 }
