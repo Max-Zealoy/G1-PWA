d73863ad281a86a113500d23af8dd13f5673676b
simonperssonnn <persson_simon@outlook.com>
2021-05-20 23:26:38 +0200

    jao

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import '../styling/UploadPage.css'
+import CatPicture from '../images/Buttons/Ted.png'
+
+
+
+
+
+
 
 function Upload() {
     return (
-        <div>
+        <div className="Uploadpicture">
             <h1>Upload the best picture of your cat MEOW!</h1>
+                <div className='Picture'>
+            <a><img src={CatPicture} alt='CatPicture' /></a>
+                </div>
             <div className="mb-1">
-     Image <span className="font-css top">*</span>
-     <div className="">
-         <input type="file" id="file-input" name="ImageStyle"/>
-     </div>
+            <span className="font-css top"></span>
+            <div className="">
+            <input type="file" id="file-input" name="ImageStyle"/>
+         </div>
 </div>
 
         </div>
