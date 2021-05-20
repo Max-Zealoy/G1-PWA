4b1d7d93b4d1fe471759740dc5a8a53a273f9877
unknown <mxzetterlund@gmail.com>
2021-05-20 17:18:22 +0200

    added css

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,22 @@
 import './LoginPage.css'
 import React from 'react'
+import {Link} from "react-router-dom";
 import cat from '../images/Buttons/catlogo.png'
 
 function LoginPage() {
+
+    
     return (
         <div id="LoginBody">
             <h2 id="TitleLogin">hello Catster!</h2>
             <img src={cat} alt="" width= "40%" />
-                <form action="LoginForm" id="LoginForm">
+            <form action="LoginForm" id="LoginForm">
            Meowstername <br /> <input type="text" id="UserName" /> <br />
             Meowword <br /><input type="text" id="PassWord" /> <br />
 
-            <input type="button" value="Login" id="LoginButton"  />
+            <Link to='../SearchPage'> <input type="button" value="Login" id="LoginButton"  />
              <br />
+             </Link>
               New user? Join our cat army 
              <br />
             <input type="button" value="Here!" id="CreateAccountButton"/>
