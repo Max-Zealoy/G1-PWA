afa5eda4ffbd972d2452bb378b8c3fa6a51e3589
simonperssonnn <persson_simon@outlook.com>
2021-05-20 17:19:20 +0200

    a

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -4,6 +4,13 @@ function Upload() {
     return (
         <div>
             <h1>Upload the best picture of your cat MEOW!</h1>
+            <div className="mb-1">
+     Image <span className="font-css top">*</span>
+     <div className="">
+         <input type="file" id="file-input" name="ImageStyle"/>
+     </div>
+</div>
+
         </div>
     )
 }
