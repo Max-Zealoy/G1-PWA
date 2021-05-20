fc3ae7ba6130c96829cc02f0d5e0da8a4557adfa
simonperssonnn <persson_simon@outlook.com>
2021-05-21 00:23:04 +0200

    Upload prototyp klar

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -11,15 +11,28 @@ import CatPicture from '../images/Buttons/Ted.png'
 function Upload() {
     return (
         <div className="Uploadpicture">
-            <h1>Upload the best picture of your cat MEOW!</h1>
-                <div className='Picture'>
+            <h2>Uploading a picture? Pawsome!</h2>
+                <div className='TedPicture'>
             <a><img src={CatPicture} alt='CatPicture' /></a>
                 </div>
             <div className="mb-1">
             <span className="font-css top"></span>
-            <div className="">
+            <div className="inputPic">
             <input type="file" id="file-input" name="ImageStyle"/>
-         </div>
+            <select value="Cat breed">
+            <option value="Orange">Maine coon</option>
+            <option value="Radish">British shorthair</option>
+            <option value="Cherry">Sphynx</option>
+            <option value="Cherry">Fat & ugly</option>
+            <option value="Cherry">Norwegian forest cat</option> 
+            </select>
+            </div>
+            <div classname="description">
+                <input type="text" id="textDescription" name="fname" 
+                value ="Look at my sweet baby! #SweetOneteen #BirthdayBoy" />
+                
+            </div>
+            <button class="button">Upload</button>
 </div>
 
         </div>
