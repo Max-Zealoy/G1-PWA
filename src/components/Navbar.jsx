f43857ecc992be893d84288a4d39df4e5acd8e74
simonperssonnn <persson_simon@outlook.com>
2021-05-20 15:20:47 +0200

    Co-authored-by: Max-Zealoy <Max-Zealoy@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import './Navbar.css'
-
+import Home from '../images/Buttons/homeButton.png'
+import Search from '../images/Buttons/searchButton.png'
+import AddBtn from '../images/Buttons/addPhotoButton.png'
+import Notification from '../images/Buttons/NotificationButton.png'
+import ProfileButton from '../images/Buttons/ProfileButton.png'
 
 function Navbar() {
 const [open, setOpen] = useState(false);
@@ -8,23 +12,32 @@ const [open, setOpen] = useState(false);
 return (
         <div>
             <nav>
-                <div className="logo">CATSNAP</div>
+                <div className="logoText"><h3>-CATSNAP-</h3></div>
+
+
                 <ul className="nav-links" 
                 style={{transform: open ? "translateX(0px)" : ""}}>
                     <li>
                         
                     </li>
                     <li>
-                        <a>Product</a>
+                        <a><img src={Home} alt='Home' /></a>
                     </li>
+                    
                     <li>
-                        <a>Services</a>
+                    <a><img src={Search} alt='Search' /></a>
                     </li>
+                    
                     <li>
-                        <a>About</a>
+                    <a><img src={AddBtn} alt='AddBtn' /></a>
                     </li>
+                    
                     <li>
-                        <a>Contact</a>
+                    <a><img src={Notification} alt='Notification' /></a>
+                    </li>
+                    
+                    <li>
+                    <a><img src={ProfileButton} alt='ProfileButton' /></a>
                     </li>
                 </ul>
              {/*   <i onClick={() => setOpen(!open)} className="fas fa-bars burger"></i> */}
