1db26ababe140ee230cc1f6445195063725a9136
unknown <mxzetterlund@gmail.com>
2021-05-21 11:46:35 +0200

    Added last quick fixes

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -6,6 +6,14 @@ import Navbar from '../components/Navbar'
 import SimpleButton from '../components/Buttons'
 
 import '../styling/ProfilePage.css'
+import cat1 from '../images/cats/cat1.jpg'
+import cat2 from '../images/cats/cat2.jpg'
+import cat3 from '../images/cats/cat3.jpg'
+import cat4 from '../images/cats/cat4.jpg'
+import cat5 from '../images/cats/cat5.jpg'
+import cat6 from '../images/cats/cat6.jpg'
+
+
 
 
 export default function ProfilePage() {
@@ -20,8 +28,7 @@ export default function ProfilePage() {
     <div className="GridProfile">
       <h1 className="Title">My Profile</h1>
       <h2 className="Notification">Push notification</h2>
-      <h2 className="Off">Off</h2>
-      <h2 className="On">On</h2>
+    
       <img className="Lady"src={Lady} alt="Avatar"></img>
       <img className="Edit"src={Edit} alt="Edit"></img> 
       <img className="NotificationBar"src={Notification} alt="Bar"></img>   
@@ -34,6 +41,16 @@ export default function ProfilePage() {
       <h2 className="UploadedPictures">Pictures</h2>
       <h2 className="FiveT">5000</h2>
 
+
+      </div>
+
+      <div className="GridProfilePictures">
+      <img className="Cat1"src={cat1} alt="Cat1"></img> 
+      <img className="Cat2"src={cat2} alt="Cat2"></img> 
+      <img className="Cat3"src={cat3} alt="Cat3"></img> 
+      <img className="Cat4"src={cat4} alt="Cat4"></img> 
+      <img className="Cat5"src={cat5} alt="Cat5"></img> 
+      <img className="Cat6"src={cat6} alt="Cat6"></img> 
 
       </div>
     </div>
